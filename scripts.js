905f1715427cf1797734588874f661add7975668
nellie445 <nelliesmart445@gmail.com>
2023-11-05 19:00:10 -0500

    Added images for webpage

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-/* Add your JavaScript to this file */
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const form = document.querySelector('#newsletter-form');
+        const emailInput = document.querySelector('#email');
+        const messageDiv = document.querySelector('.message');
+
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+
+            const email = emailInput.value;
+
+            if (email.trim() === '') {
+                messageDiv.textContent = 'Please enter a valid email address.';
+            } else {
+                messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+            }
+        });
+    });
+</script>
+
+
